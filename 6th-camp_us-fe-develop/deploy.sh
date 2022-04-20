4d50ee8b600530ff167c3db31b1602246f3db517
정호진 <101921485+Jeonghojin8@users.noreply.github.com>
2022-04-20 12:22:41 +0900

    Delete 6th-camp_us-fe-develop directory

--- 6th-camp_us-fe-develop/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "> FE 배포"
-sudo cp -rf /home/ubuntu/deploy-fe/dist/* /var/www/html
--- 6th-camp_us-fe-develop/src/pages/communityPage/communityWritePage/communityWritePage.style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Container = styled.section`
-  background-color: #fafafa;
-`;
-
-export const style = {
-  Container,
-};
